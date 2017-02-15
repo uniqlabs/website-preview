218298e8d5694361b5f2ade0fbda5d3c3a39cea2
Klaas <klaas@loclet.com>
2017-02-15 22:04:22 +0100

    Update 2017-02-15_22:04:22

--- assets/js/apply.js
+++ assets/js/apply.js
@@ -6,10 +6,20 @@ $(document).ready(function () {
         params[item.split("=")[0]] = item.split("=")[1]
     });
 
+    // Pre-fill inputs if parameters were passed in
     if (params.e && !isNaN(params.e))
         $('#experience').val(params.e);
     if (params.t && !isNaN(params.t))
         $('#time').val(params.t);
     if (params.a && !isNaN(params.a))
         $('#availability').val(params.a);
+
+    $('#apply').validator().on('submit', function (e) {
+        if (e.isDefaultPrevented()) return;
+        e.preventDefault();
+    });
+
+    // window.onbeforeunload = function() {
+    //     return "Are you sure you want to navigate away?";
+    // }
 });
